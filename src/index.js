094a69e89d97f6a694165169665f7af749fe17f1
Shubham Garhewal <96170508+shubhamgarhewal@users.noreply.github.com>
2022-08-16 22:47:44 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/SignUp";
+import Products from "./components/products/Products";
+
+import Details from "./components/Details/Details";
+import CreateOrder from "./components/Details/CreateOrder";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signUp" element={<SignUp></SignUp>}></Route>
+        <Route path="/details" element={<Details></Details>}></Route>
+        <Route
+          path="/createOrder"
+          element={<CreateOrder></CreateOrder>}
+        ></Route>
+        <Route path="/" element={<Products></Products>}></Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
